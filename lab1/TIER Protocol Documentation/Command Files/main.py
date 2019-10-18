fd2e508226841becd32ad8a341536fe962f8b9df
rafalsiniewicz <36672426+rafalsiniewicz@users.noreply.github.com>
2019-10-18 21:01:12 +0200

    Add files via upload

--- lab1/TIER Protocol Documentation/Command Files/main.py
+++ lab1/TIER Protocol Documentation/Command Files/main.py
@@ -1,1 +1,8 @@
-
+from pandas import *
+df = read_csv('drinks.csv')
+print(df)
+df = melt(df, id_vars =['country'], 
+	value_vars =['beer_servings', 'spirit_servings', 'wine_servings', 'total_litres_of_pure_alcohol'],
+	var_name ='type',
+	value_name ='litres') 
+print(df)
